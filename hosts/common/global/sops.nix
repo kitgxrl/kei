97d06c0d17e9c065aa192723266d01aaf1093fa8
Rain <kitgxrl@gmail.com>
2023-10-11 23:05:10 -0400

    fix(sops): Capitalization

--- hosts/common/global/sops.nix
+++ hosts/common/global/sops.nix
@@ -1,10 +1,10 @@
 { inputs, config, ... }:
 let
-  ised25519 = k: k.type == "ed25519";
-  getkeypath = k: k.path;
-  keys = builtins.filter ised25519 config.services.openssh.hostkeys;
+  isEd25519 = k: k.type == "ed25519";
+  getKeyPath = k: k.path;
+  keys = builtins.filter isEd25519 config.services.openssh.hostKeys;
 in {
-  imports = [ inputs.sops-nix.nixosmodules.sops ];
+  imports = [ inputs.sops-nix.nixosModules.sops ];
 
-  sops = { age.sshkeypaths = map getkeypath keys; };
+  sops = { age.sshKeyPaths = map getKeyPath keys; };
 }
