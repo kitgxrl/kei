9ad4e9cf97984c4dbb7e7747cca71c4933ebce23
Rain <kitgxrl@gmail.com>
2023-10-29 13:26:59 -0400

    feat(optional): Add pipewire

---
+++ hosts/common/optional/pipewire.nix
@@ -0,0 +1,14 @@
+{ ... }: {
+  hardware.pulseaudio.enable = true;
+
+  services.pipewire = {
+    enable = true;
+
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+
+    pulse.enable = true;
+  };
+}
